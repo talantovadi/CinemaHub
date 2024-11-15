9b19392f7b622181113fd03729055e12dd23b655
talantovadi <dilbartalantova@gmail.com>
2024-11-15 10:22:22 +0600

    Added link to presentation and whitelist endpoints

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CinemaHub
 This is a repo for api for managing different films and actors
+
+
+[Ссылка на презентацию ](https://www.canva.com/design/DAGWg6C-CFM/F73MSCZNQL8pruYUMLknJQ/edit)
--- src/main/java/kg/talantova/cinemahub/config/WebSecurityConfig.java
+++ src/main/java/kg/talantova/cinemahub/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import kg.talantova.cinemahub.repository.UserRepository;
 import kg.talantova.cinemahub.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -22,6 +23,12 @@ public class WebSecurityConfig {
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
 
+    private final String[] WHITELISTED_ENDPOINTS = {
+            "/api/films/**",
+            "/api/actors/**",
+            "/api/users/**"
+    };
+
     public WebSecurityConfig(UserRepository userRepository, JwtAuthenticationFilter jwtAuthFilter, AuthenticationProvider authenticationProvider) {
         this.userRepository = userRepository;
         this.jwtAuthFilter = jwtAuthFilter;
@@ -34,7 +41,7 @@ public class WebSecurityConfig {
                 .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
-//                        .requestMatchers(HttpMethod.GET, "/api/films/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, WHITELISTED_ENDPOINTS).permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
